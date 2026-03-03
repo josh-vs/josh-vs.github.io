41b8c1c582f82cab3bd38eed03dde101da7d0142
Josh A. Lilleberg <joshua.lilleberg@synergine.cz>
2026-03-03 20:05:53 +0100

    Reconfigured eleventy and tailwind and added fluid-tailwind plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,32 +1,34 @@
-const { DateTime } = require("luxon");
-const pluginSEO = require("eleventy-plugin-seo");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const markdownIt = require("markdown-it");
-const markdownItClass = require('@toycode/markdown-it-class');
-const tailwind = require("tailwindcss");
-const postCss = require("postcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+import tailwindcss from 'tailwindcss';
+import postcss from 'postcss';
+import cssnano from 'cssnano';
+import autoprefixer from 'autoprefixer';
+import markdownIt from 'markdown-it';
+import markdownItClass from '@toycode/markdown-it-class';
+import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
+import pluginSEO from 'eleventy-plugin-seo';
+import seo from './src/seo.json' with { type: 'json'};
+import { DateTime } from 'luxon';
 
+const postcssFilter = async (cssCode, done) => {
+  try {
+    const result = await postcss([
+      tailwindcss('./tailwind.config.js'),
+      autoprefixer(),
+      cssnano({ preset: "default" }),
+    ]).process(cssCode, { from: "./assets/tailwind.css" });
 
-const postcssFilter = (cssCode, done) => {
-  postCss([
-    tailwind(require("./tailwind.config")),
-    autoprefixer(),
-    cssnano({ preset: "default" }),
-  ])
-    .process(cssCode, {
-      from: "./assets/tailwind.css",
-    })
-    .then(
-      (r) => done(null, r.css),
-      (e) => done(e, null)
-    );
+    done(null, result.css);
+  } catch (err) {
+    done(err, null);
+  }
 };
 
-
-
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginSEO, seo);
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("assets/js");
+  eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
   eleventyConfig.setTemplateFormats([
     "html",
     "njk",
@@ -37,72 +39,42 @@ module.exports = function (eleventyConfig) {
     "JPG",
     "png",
   ]);
-
   eleventyConfig.addNunjucksFilter("date", (dateString) => {
-    dateObj = new Date(dateString);
+    const dateObj = new Date(dateString);
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLLL yyyy');
   });
-
   eleventyConfig.addNunjucksFilter("fulldate", (dateString) => {
-    dateObj = new Date(dateString);
+    const dateObj = new Date(dateString);
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('d LLLL yyyy');
   });
-
-  eleventyConfig.addNunjucksFilter("getLatest", function(collection) {
+  eleventyConfig.addNunjucksFilter("getLatest", function (collection) {
     return collection.sort((a, b) => b.date - a.date)[1];
   });
-
-  eleventyConfig.addPlugin(syntaxHighlight);
-
-  eleventyConfig.addPassthroughCopy("assets");
-
-  eleventyConfig.addPassthroughCopy("assets/js");
-
-  const seo = require("./src/seo.json");
-
-  eleventyConfig.addPlugin(pluginSEO, seo);
-
-  eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
-
-  eleventyConfig.addCollection("posts", addCollection("posts"));
-
-  // Configure Markdown-it
   const mdOptions = {
     html: true, // Allow HTML in Markdown
     breaks: true, // Turn line breaks into <br>
     linkify: true, // Autoconvert URLs to links
   };
-
-
-  const md = markdownIt(mdOptions);
-
-  // Set Markdown-it as Eleventy's Markdown processor
-  eleventyConfig.setLibrary("md", md);
-
-  // Tailwind & postcss config:
+  eleventyConfig.setLibrary("md", markdownIt(mdOptions));
   eleventyConfig.addWatchTarget("./assets/tailwind.css");
   eleventyConfig.addNunjucksAsyncFilter("postcss", postcssFilter);
-
+  eleventyConfig.addCollection("posts", addCollection("posts"));
   function addCollection(collectionName) {
     return function (collection) {
       const coll = collection
         .getFilteredByTag(collectionName)
         .sort((a, b) => b.data.date - a.data.date);
-
       // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
       // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
       for (let i = 0; i < coll.length; i++) {
         const prevPost = coll[i - 1];
         const nextPost = coll[i + 1];
-
         coll[i].data["prevPost"] = prevPost;
         coll[i].data["nextPost"] = nextPost;
       }
-
       return coll;
     };
   };
-
 
   return {
     passthroughFileCopy: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "src/**/*.{njk, md, html}"
-  ],
+import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+
+export default {
+  content: {
+    files: ["src/**/*.{njk, md, html}"],
+    extract
+  },
   safelist: [],
   darkMode: "class",
   theme: {
@@ -12,11 +15,16 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [fluid,
+            require("@tailwindcss/typography")],
   theme: {
+    screens, // Tailwind's default screens, in `rem`Q
+    fontSize, // Tailwind's default font sizes, in `rem` (including line heights)
     extend: {
+      screens: {
+        xs: '20rem'
+      },
       fontFamily: {
-        victor: ['"Victor Mono"'],
         garamond: ['"Cormorant Garamond"'],
       },
       colors: {
@@ -27,10 +35,8 @@ module.exports = {
         'tlorp': "#0f2726",
         'plorp': "#c2a6bf",
         'brap': "#1f3f3d",
-        'brap_white': "#85919b"
-      }
+        'brap_white': "#85919b"      }
     },
   },
 };
-
 
